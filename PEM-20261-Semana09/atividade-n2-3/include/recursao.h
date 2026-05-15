0b2fbf5d4ff8cc5f9c1777da24d9dce27931fcd8
Vinícius_M. <131198175+vinny-mariano@users.noreply.github.com>
2026-05-15 00:17:09 -0300

    Delete PEM-20261-Semana09/atividade-n2-3 directory

--- PEM-20261-Semana09/atividade-n2-3/include/recursao.h
+++
@@ -1,9 +0,0 @@
-#ifndef RECURSAO_H
-#define RECURSAO_H
-
-    float som(float *a, float *b);
-    float sub(float *a, float *b);
-    float mult(float *a, float *b);
-    float div(float *a, float *b);
-
-#endif
