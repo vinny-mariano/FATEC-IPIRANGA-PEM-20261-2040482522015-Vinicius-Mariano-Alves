e0948cc6963fc34a043559fc1f256867f1983738
Vinícius_M. <131198175+vinny-mariano@users.noreply.github.com>
2026-05-29 11:06:01 -0300

    Add conversion program from cm to m, km, and miles

---
+++ PEM-20261-Semana14/ex02.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    int cm;
+    float m;
+    double km, mile;
+
+    printf("Informe um valor positivo em centimetros para ser convertido\n VALOR LIMITE (2.147.483.647)\n");
+    scanf("%d", &cm);
+
+    while(cm < 0 || cm > 2147483647){
+        printf("Informe um valor positivo em centimetros dentro do valor limite!");
+        scanf("%d", &cm); 
+    }
+    
+    //O uso do while garante que o programa continue ativo aceitndo apenas valores que cumpram com a regra estabelecida 
+
+    m = cm / 100.0;
+    km = m /1000.0;
+    mile = km / 1.60934;
+
+    printf("O valor informado em metros e: %.2f", m);
+    printf("O valor informado em quilometros é: %.2lf", km);
+    printf("O valor informado em milhas e: %.2lf", mile);
+
+
+    return 0;
+}
