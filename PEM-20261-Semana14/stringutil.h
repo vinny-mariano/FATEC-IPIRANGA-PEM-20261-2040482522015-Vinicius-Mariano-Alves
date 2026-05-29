fd831f8fa40f324eb412f956929c9e8cd1b11ee5
Vinícius_M. <131198175+vinny-mariano@users.noreply.github.com>
2026-05-29 11:42:15 -0300

    Create stringutil.h

---
+++ PEM-20261-Semana14/stringutil.h
@@ -0,0 +1,18 @@
+/*----------------------------------------------------------------------------------*
+ * - Nome: Vinícius Mariano Alves                                                   *
+ * - Fatec Ipiranga - ADS Vespertino                                                *
+ * - Professor: Veríssimo                                                           *
+ * - Disciplina: Programação Estruturada e Modular (PEM)                            *
+ * - Atividade: Semana 14 - Exercício 13 (.h)                                       *
+ * - Objetivo: Protótipos e assinaturas das funções de manipulação de strings       *
+ * para o módulo de utilitários.                                                    *
+ *----------------------------------------------------------------------------------*/
+
+#ifndef STRINGUTIL_H
+#define STRINGUTIL_H
+
+int contaVogais(char *s);
+void inverteCString(char *s);
+int ePalindromo(char *s);
+
+#endif
