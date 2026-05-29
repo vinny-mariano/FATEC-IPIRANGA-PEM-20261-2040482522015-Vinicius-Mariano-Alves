9d1c686b9af34106a77ddbb59a8423681dc964ce
Vinícius_M. <131198175+vinny-mariano@users.noreply.github.com>
2026-05-29 10:51:07 -0300

    Correção scanf, logica de calculo e adição de sizeof

--- PEM-20261-Semana14/ex01.c
+++ PEM-20261-Semana14/ex01.c
@@ -2,33 +2,38 @@
 
 int main(){
 
-    int p1, p2, p3;
-    double n1, n2, n3, media;
+        int p1, p2, p3;
+        double n1, n2, n3, media;
 
-printf("INSIRA O PESO E O VALOR DA PRIMEIRA NOTA\n");
-printf("PESO:");
-scanf("%d", &p1);
-printf("\n");
-printf("NOTA:");
-scanf("%lf", &n1);
+    printf("INSIRA O PESO E O VALOR DA PRIMEIRA NOTA\n");
+    printf("PESO:");
+    scanf("%d", &p1);
+    printf("\n");
+    printf("NOTA:");
+    scanf("%lf", &n1);
 
-printf("INSIRA O PESO E O VALOR DA SEGUNDA NOTA\n");
-printf("PESO:");
-scanf("%d", &p2);
-printf("\n");
-printf("NOTA:");
-scanf("%lf", &n2);
+    printf("INSIRA O PESO E O VALOR DA SEGUNDA NOTA\n");
+    printf("PESO:");
+    scanf("%d", &p2);
+    printf("\n");
+    printf("NOTA:");
+    scanf("%lf", &n2);
 
-printf("INSIRA O PESO E O VALOR DA TERCEIRA NOTA\n");
-printf("PESO:");
-scanf("%d", &p3);
-printf("\n");
-printf("NOTA:");
-scanf("%lf", &n3);
+    printf("INSIRA O PESO E O VALOR DA TERCEIRA NOTA\n");
+    printf("PESO:");
+    scanf("%d", &p3);
+    printf("\n");   
+    printf("NOTA:");
+    scanf("%lf", &n3);
 
-    media = (n1*p1) + (n2*p2) + (n3*p3) / (p1 + p2 + p3);
+        media = (n1*p1 + n2*p2 + n3*p3) / (p1 + p2 + p3);
 
-printf("A media ponderada considerando os pesos e os valores de cada nota informada e:\n %lf", media);
+    printf("A media ponderada considerando os pesos e os valores de cada nota informada e:\n %.2lf\n", media);
+
+    printf("Tamanho da variável int: %zu bytes\n", sizeof(int));
+
+    printf("Tamanho da variável double: %zu bytes\n", sizeof(double));
+
 
     return 0;
 }
